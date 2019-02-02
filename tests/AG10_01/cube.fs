251c7d863dfbd8ee8e87273107200ea67d32fe62
Julia Penades <julia_penades@hotmail.com>
2019-02-02 11:48:47 +0100

    AG10_01 Completed

---
+++ tests/AG10_01/cube.fs
@@ -0,0 +1,40 @@
+#version 330 core
+out vec4 fragColor;
+
+in vec3 normal;
+in vec3 fragPos;
+in vec2 texCoords;
+
+struct Material {
+	sampler2D diffuse;
+	sampler2D specular;
+	float shininess;
+};
+uniform Material material;
+
+struct Light {
+	vec3 position;
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+uniform Light light;
+
+uniform vec3 viewPos;
+
+void main(){
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, texCoords));
+	
+	vec3 norm = normalize(normal);
+	vec3 lightDir = normalize(light.position - fragPos);
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = diff * vec3(texture(material.diffuse, texCoords)) * light.diffuse;
+	
+	vec3 viewDir = normalize(viewPos - fragPos);
+	vec3 reflectDir = reflect(-lightDir, norm);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+	vec3 specular = spec * vec3(texture(material.specular, texCoords)) * light.specular;
+	
+	vec3 phong = ambient + diffuse + specular;
+	fragColor = vec4(phong, 1.0);
+}
---
+++ tests/AG10_02/stencil.vs
@@ -0,0 +1,12 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoord;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
+
+void main() {
+	gl_Position = proj * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0); 
+}
